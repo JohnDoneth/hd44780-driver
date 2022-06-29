1a480203c7e42dbe8be934d4bfe1a03644eace22
Chevdor <chevdor@gmail.com>
2022-06-29 23:03:19 +0200

    Fix #38

---
+++ src/display_size.rs
@@ -0,0 +1,32 @@
+pub struct DisplaySize {
+	columns: u8,
+	lines: u8,
+}
+
+impl DisplaySize {
+	pub fn new(columns: u8, lines: u8) -> Self {
+		Self { columns, lines }
+	}
+
+	pub fn get(&self) -> (u8, u8) {
+		(self.columns, self.lines)
+	}
+}
+
+impl Default for DisplaySize {
+	fn default() -> Self {
+		Self { columns: 20, lines: 4 }
+	}
+}
+
+#[cfg(test)]
+mod test_display_size {
+	use super::*;
+
+	#[test]
+	fn test_default_get() {
+		let std = DisplaySize::default();
+		assert_eq!(20, std.get().0);
+		assert_eq!(4, std.get().1);
+	}
+}
